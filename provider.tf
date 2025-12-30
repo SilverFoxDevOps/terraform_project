d13b30fe277f5c2d3f5b69ff242e354b263c9bbd
Rakesh Kumar Routray <45137173+SilverFoxDevOps@users.noreply.github.com>
2025-12-30 16:43:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -24,6 +24,7 @@ terraform {
     storage_account_name = "tfstate1876"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_azuread_auth     = true
   }
 }
 
